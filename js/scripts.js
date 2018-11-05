712f0cab098a6abca4e0f55fb7366e7cce0de393
AlexDominguez <alexdominguezwebdev@gmail.com>
2018-11-05 15:32:51 -0800

    modified constructor and prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,28 @@
+//Business logic
 
+function Pizza(size,toppings){
+  this.size = size;
+  this.toppings = toppings;
+  this.cost = 0.00;
+}
+
+
+Pizza.prototype.costCalculator = function(topping) {
+  var pizzaPrice = 0;
+  if (this.size === "small") {
+    pizzaPrice = 6.00;
+  } else if (this.size === "medium") {
+    pizzaPrice = 8.00;
+  } else if (this.size === "large") {
+    pizzaPrice = 10.00;
+  } else if (this.size === "extra-large") {
+    pizzaPrice = 12.00;
+  }
+
+
+
+
+
+
+
+//User-Interface Logic
